23edec56820b618045ef62edb7a7b904fda4a129
Jules Bleijenberg <jbleijen@f0r2s6.codam.nl>
2025-03-05 15:00:21 +0100

    Init

---
+++ main.sh
@@ -0,0 +1,228 @@
+#! /usr/bin/bash
+
+# Print line :]
+print_line () {
+	printf "$@\n";
+}
+
+# Copmile c files then run and remove ./a.out
+run_cc () { 
+	if [[ $use_raw_cc == 1 ]]; then
+		cc $* && ./a.out && rm ./a.out & pid=$!;
+	else
+		cc $* -Wall -Wextra -Werror && ./a.out && rm ./a.out & pid=$!;
+	fi
+	# Timeout for infinite loops
+	for i in {1..55}; do
+		read -n 1 -p "" -t 0.1 should_kill;
+		if [[ $should_kill == "q" ]]; then
+			print_line "uit process. \nTrace: terminated by the terminator";
+			kill $pid;
+		fi
+		out=$( ps -p "$pid" --format=%p );
+		if [[ ! $out == *"$pid"* ]]; then
+			return;
+		fi
+	done
+	out=$( ps -p "$pid" --format=%p );
+	if [[ $out == *"$pid"* ]]; then
+		print_line "Exit by timeout";
+		kill $pid;
+	fi
+}
+
+# Check global exercises
+analyse_all () {
+	clear;
+	print_line "\n--- NORMINETTE:";
+	norminette -R CheckForbiddenSourceHeader $folder_path/../;
+	print_line "\n--- TODO:";
+	print_line "Did you push your code to git???";
+}
+
+# Check exercises
+analyse () {
+	clear;
+	print_line "\n--- NORMINETTE:";
+	norminette -R CheckForbiddenSourceHeader $exercises_path;
+	print_line "\n--- TODO:";
+	print_line "Did you push your code to git???";
+}
+
+# Program variables
+main_path=~/".easy_run";
+joined_arguments="";
+
+# Colors
+GREEN='\033[0;32m';
+NO_COLOR='\033[0m';
+
+# Program options
+options="hauDtdr"; 
+is_clearing=1;
+is_downloading=0;
+is_testing=0;
+use_raw_cc=0;
+is_developing=0;
+
+# Get the options
+while getopts $options option;
+do
+	 case $option in
+		h) # display Help
+			help_file="$main_path/help.txt";
+			cat $help_file;
+			exit;;
+		a)
+			analyse
+			exit;;
+		u) # Don't clear | u stands for unclear or useless considering I'll never use it :] 
+			is_clearing=0;;
+		D) # Is downloading
+			is_downloading=1;;
+		t) # Is testing
+			is_testing=1;;
+		d) # Is developing
+			is_developing=1;;
+		r) # Set use raw cc
+			use_raw_cc=1;;
+	esac
+done
+
+# Get non-option arguments (e.g. /home/usr/exercises/c_00/ft_putchar.c, ...)
+count=0;
+for arg in  $@;
+do
+	if [[ ! "$arg" =~ ^- ]]; then
+		joined_arguments+="$arg ";
+		count=count+1;
+	fi
+done
+
+# Funny error message
+if [ $count == 0 ]; then
+	if [ $is_downloading -eq 1 ]; then
+		print_line "No Dave, we don't magically know which link you want to use";
+	else
+		print_line "No Dave, we don't magically know which file or folder you want to use";
+	fi
+	exit;
+fi
+
+# Download files from intra
+if [ $is_downloading -eq 1 ]; then
+	print_line "Downloading files from $joined_arguments";
+	wget -q -O - $joined_arguments | pdftotext - $main_path/tmp_subject.txt
+
+	folders=($(awk 'BEGIN {directory="c_00";} \
+	{if ($1 == "Turn-in") printf "%s/Exercises/%s/exercises/%s\n", ENVIRON["HOME"], directory, $4; \
+	else if (NR==2) directory=gensub(/ /, "_", "g", tolower($0))}' $main_path/tmp_subject.txt))
+
+	files=($(awk 'BEGIN {directory="c_00"; current_exercise="ex00/"} \
+	{if ($1 == "Turn-in") current_exercise=$4; \
+	else if ($1 == "Files") printf "%s/Exercises/%s/exercises/%s%s\n", ENVIRON["HOME"], directory, current_exercise, $6; \
+	else if (NR==2) directory=gensub(/ /, "_", "g", tolower($0))}' $main_path/tmp_subject.txt))
+	rm $main_path/tmp_subject.txt
+
+	echo "---Folders"
+	for folder in ${folders[@]}; do
+		echo "$folder"
+	done
+	echo ""
+	echo "---Files"
+	for file in ${files[@]}; do
+		echo "$file"
+	done
+	read -n 1 -p "Is structure correct? (y/n)" user_input
+	echo ""
+	if [[ $user_input == "y" ]]; then
+		for str in ${folders[@]}; do 
+			mkdir -p $(echo "$str" | sed 's/.\{1\}$//')
+			# sed removes last char, because bash programming is bash programming
+		done
+		for file in ${files[@]}; do 
+			# TO DO - Add 42 header
+			touch $file
+		done
+		echo -e "${GREEN}Files created"
+	fi
+	exit;
+fi
+
+given_path="$( cut -d ' ' -f 1 <<< "$joined_arguments" )";
+given_path=$(readlink -f "$given_path");
+# If path doesn't end with .c -> add every .c file in folder
+if [[ -d $given_path ]]; then
+	folder_path=$given_path;
+	given_path="${given_path}/*.c";
+fi
+
+# Set exercises and tests path based on given path
+if [[ $given_path =~ .*"exercises".* ]]; then
+	exercises_path=${given_path};
+	tests_path=$(readlink -f "$given_path" | sed -e 's/exercises/tests/g');
+else
+	tests_path=${given_path};
+	exercises_path=$(readlink -f "$given_path" | sed -e 's/tests/exercises/g');
+fi
+
+open_actions () {
+	# Log file options
+	print_line "\n --- ACTION";
+	print_line "1) Open exercise files";
+	print_line "2) Open test files";
+	print_line "a) Analyse parent folder";
+	print_line "f) Norminette file";
+	print_line "r) Rerun program";
+	print_line "q) Exit";
+	read  -n 1 -p "File selection:" user_input;
+	case $user_input in
+		1)
+			vim $exercises_path $tests_path;;
+		2)
+			vim $tests_path $exercises_path;;
+		a)
+			analyse_all
+			open_actions;;
+		f)
+			analyse 
+			open_actions;;
+		r)
+			clear;;
+		q)
+			clear
+			exit;;
+		*)
+			exit;;
+	esac
+}
+
+run_cc_and_log () {
+	if [ $is_clearing -eq 1 ]; then
+		clear;
+	fi
+	print_line " --- FILES:";
+	if [ $is_testing -eq 1 ]; then
+		print_line "$exercises_path - $tests_path";
+		print_line "\n --- TEST FILE:";
+		cat $tests_path;
+		print_line "\n --- OUTPUT";
+		run_cc $exercises_path $tests_path;
+	else
+		print_line "$exercises_path";
+		print_line "\n --- OUTPUT";
+		run_cc $exercises_path;
+	fi
+	print_line "\n";
+	if [ $is_developing -eq 1 ]; then
+		open_actions;
+		run_cc_and_log;
+	fi
+}
+
+# Check if file is empty
+if [ ! -s $given_path ]; then
+	vim $exercises_path $tests_path;
+fi
+
+run_cc_and_log
